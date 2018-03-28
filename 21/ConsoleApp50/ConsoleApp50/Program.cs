25c527ef2b87ea03f6d880ec790536aa77edd494
Anzerus <Anzerus@mail.ru>
2018-03-28 18:19:03 +0300

    123
    123

---
+++ 21/ConsoleApp50/ConsoleApp50/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i < 200; i++)
+            {
+               
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+          
+        }
+    }
+}
